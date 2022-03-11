9ecec39d79ce46538d3e24d6f704598b57c992f1
Rodrigo Gomes <rodrigo.gomes@moovenconsulting.com.br>
2022-03-11 17:58:26 -0300

    creation of private routes, fallback added partially at didactic level

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+
+Route::get('/login', function () { return 'Login';})->name('site.login');
+
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function () {return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function () {return 'Produtos';})->name('app.produtos');
+});
+
+Route::fallback(function(){
+    echo 'Esta pagina não existe, <a href="'.route('site.index').'">clique aqui</a> para voltar a página principal.';
+});
